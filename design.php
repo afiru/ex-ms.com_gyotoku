3e29fa87ca94e1bb0b606f155d9b12d3eca06d5e
afiru <yfb19369@gmail.com>
2020-04-16 13:48:38 +0900

    画像追加

--- design.php
+++ design.php
@@ -4,38 +4,37 @@
     <div class="text_box">
         <h1 class="t_center color_fff mintyou">DESIGN</h1>
         <p class="mintyou color_fff t_center main_plan_contenr_contents_p">
-            街の品格に映える誇りある私邸
+            駅チカでありながら、<br>公園の潤いに包まれる贅沢。
         </p>
     </div>
 </section>
-<figure class="top_page_design margin_100 he_1007_2028_330_4vw"></figure>
-<main class="wapper margin_100 top_page_design_contentnts">
-    <figure class="design_landp_plan">
-        <picture>
-            <source class="lazy" media="(min-width: 768px)" data-srcset="img/design/design_landp_plan_pc.png"><!--PC画像-->
-            <source class="lazy"  media="(max-width: 768px)" data-srcset="img/design/design_landp_plan_sp.png"><!--SP画像-->
-            <img class="lazy" data-src="img/design/design_landp_plan_pc.png" alt="" />
-        </picture>
-    </figure>
-    <figure class="design_innner_corridor">
-        <picture>
-            <source class="lazy" media="(min-width: 768px)" data-srcset="img/design/design_innner_corridor_pc.png"><!--PC画像-->
-            <source class="lazy"  media="(max-width: 768px)" data-srcset="img/design/design_innner_corridor_sp.png"><!--SP画像-->
-            <img class="lazy" data-src="img/design/design_innner_corridor_pc.png" alt="" />
-        </picture>
-    </figure>
-</main>
-<aside class="back_E0DEBD margin_70 padding_top_70_bottom_90 design_bottom_sec">
-    <div class="wapper design_bottom_sec_wap">
-        <figure class="design_bottom_sec_wap_img">
-            <picture>
-                <source class="lazy" media="(min-width: 768px)" data-srcset="img/design/design_bottom_sec_wap_img_pc.png"><!--PC画像-->
-                <source class="lazy"  media="(max-width: 768px)" data-srcset="img/design/design_bottom_sec_wap_img_sp.png"><!--SP画像-->
-                <img class="lazy" data-src="img/design/design_bottom_sec_wap_img_pc.png" alt="" />
-            </picture>
-        </figure>
+<figure class="top_page_design he_1007_2028_330_4vw"></figure>
+<main class="margin_60 top_page_design_contentnts">
+    <div class="design_landp_plan padding_top_40_bottom_10">
+        <div class="wapper design_landp_plan_wap">
+            <figure class="design_landp_plan_figure">
+                <picture>
+                    <source class="lazy" media="(min-width: 768px)" data-srcset="img/design/design_landp_plan_pc.png"><!--PC画像-->
+                    <source class="lazy"  media="(max-width: 768px)" data-srcset="img/design/design_landp_plan_sp.png"><!--SP画像-->
+                    <img class="lazy" data-src="img/design/design_landp_plan_pc.png" alt="" />
+                </picture>
+            </figure>
+        </div>
     </div>
-</aside>
+    <div class="design_innner_corridor margin_30">
+        <div class="wapper design_innner_corridor_wap">
+            <p>※掲載の建物完成予想CG・敷地配置図は計画段階の図面を基に描いたもので実際とは異なります。なお、外観・内観の細部・設備機器・配管類等は一部省略又は簡略化しております。植栽につきましては特定の季節の状況を表現したものではなく、竣工時には完成予想図程度には成長しておりません。樹種は一部変更になる場合があります。タイルや各種部材につきましては実物と質感・色等の見え方が異なる場合があります。また今後変更になる場合があります。※敷地配置図上に表現された行徳駅前公園はイメージです。実際とは異なります。
+/<p>
+            <figure class="design_innner_corridor">
+                <picture>   
+                    <source class="lazy" media="(min-width: 768px)" data-srcset="img/design/design_innner_corridor_pc.png"><!--PC画像-->
+                    <source class="lazy"  media="(max-width: 768px)" data-srcset="img/design/design_innner_corridor_sp.png"><!--SP画像-->
+                    <img class="lazy" data-src="img/design/design_innner_corridor_pc.png" alt="" />
+                </picture>
+            </figure>
+        </div>
+    </div>
+</main>
     <nav class="margin_80 wapper index_top_nav location_bottom_nav">
         <ul class="display_flex_stretch display_row index_top_nav_ul">
             <li class="index_top_nav_ul_li">
