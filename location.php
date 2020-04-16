2bf8b42af313a1a50255d66515c0130cd6221f3e
曽我 <n.soga@sunplacom>
2020-04-16 14:33:52 +0900

    d

--- location.php
+++ location.php
@@ -1,64 +1,63 @@
 <?php include 'include/header.php'; ?>
 <?php include 'include/tuijuu_sp.php'; ?>
 <section class="display_flex_center under_page_contents under_page_contents_location">
-    <div class="text_box">
-        <h1 class="t_center color_fff mintyou">LOCATION</h1>
-        <p class="mintyou color_fff t_center main_plan_contenr_contents_p">
-            緑溢れる自然と利便性の高い商業施設
+    <div class="wapper under_page_contents_location_wap">
+        <h1 class=" color_fff mintyou under_page_contents_location_h1">LOCATION</h1>
+        <p class="mintyou color_fff  under_page_contents_location_pref">
+            駅、ショッピング、<br class="sp_only">学校、保育施設、豊かな緑。<br>あらゆる憩いが身近に揃う。
         </p>
     </div>
 </section>
-<main class="margin_90 location_main">
-    <figure class="wapper location_main_top_logo_figure">
-        <img class="lazy location_main_top_logo_figure_img" data-src="img/location/location_main_top_logo_figure.svg">
-    </figure>
-
-    <section class="uruoino_location he_998_887"></section>
-    <section class="uruoino_location_02 he_1010_2028"></section>
-
-    <p class="t_center pc_only mintyou lacation_uroino_pref">※距離分数はすべて現地からのもので1分=80mで算出したものです。</p>
-    <figure class="wapper location_main_doyadoya">
-        <picture>
-            <source class="lazy" media="(min-width: 768px)" data-srcset="img/location/location_main_doyadoya_pc.png"><!--PC画像-->
-            <source class="lazy"  media="(max-width: 768px)" data-srcset="img/location/location_main_doyadoya_sp.png"><!--SP画像-->
-            <img class="lazy" data-src="img/location/location_main_doyadoya_pc.png" alt="駅の路線図">
-        </picture>
-    </figure>
-
-    <section class="location_life he_2698_3262"></section>
-
-    <figure class="wapper location_public">
-        <picture>
-            <source class="lazy" media="(min-width: 768px)" data-srcset="img/location/location_public_pc.jpg"><!--PC画像-->
-            <source class="lazy"  media="(max-width: 768px)" data-srcset="img/location/location_public_sp.jpg"><!--SP画像-->
-            <img class="lazy" data-src="img/location/location_public_pc.jpg" alt="駅の路線図">
-        </picture>
-    </figure>
-
-    <section class="margin_130 location_education he_1872_2361"></section><!--pc1871.206  sp2361.145-->
-
-    <figure class="wapper location_life_info">
-        <picture>
-            <source class="lazy" media="(min-width: 768px)" data-srcset="img/location/location_life_info_pc.png"><!--PC画像-->
-            <source class="lazy"  media="(max-width: 768px)" data-srcset="img/location/location_life_info_sp.png"><!--SP画像-->
-            <img class="lazy" data-src="img/location/location_life_info_pc.png" alt="駅の路線図">
-        </picture>
-    </figure>
-
-
-    <nav class="margin_80 wapper index_top_nav location_bottom_nav">
-        <ul class="display_flex_stretch display_row index_top_nav_ul">
-            <li class="index_top_nav_ul_li">
-                <a href="https://www.ex-ms.com/sendmail/sendmail01.php?no=2070" target="_blank">
-                    <img src="img/location/button_index_top_nav_ul_li_01.svg" alt="資料請求">
-                </a>
-            </li>
-            <li class="index_top_nav_ul_li">
-                <a href="https://airrsv.net/AKR6399203893/calendar" target="_blank">
-                    <img src="img/location/button_index_top_nav_ul_li_02.svg" alt="お電話でのお問合せは「エクセレントシティ南行徳1丁目」ゲストサロン受付時間：10：00～19：00火・水曜定休（祝日は除く）">
-                </a>
-            </li>
-        </ul>
-    </nav>
+<main class="location_main">
+    <section class="location_main_contetnts_01">
+        
+    </section>
+    <section class="margin_60 location_main_contetnts_02">
+        
+    </section>
+    <section class="margin_60 location_main_contetnts_03">
+        
+    </section>
+    <section class="margin_60 location_main_contetnts_04">
+        
+    </section>
+    <section class="margin_60 location_main_contetnts_05">
+        
+    </section>
 </main>
+
+<nav class="margin_80 wapper index_top_nav">
+    <ul class="display_flex_stretch display_row index_top_nav_ul">
+        <li class="index_top_nav_ul_li">
+            <a href="https://www.ex-ms.com/sendmail/sendmail01.php?no=2110" target="_blank">
+                <img src="img/index/button_index_top_nav_ul_li_01.svg" alt="資料請求">
+            </a>
+        </li>
+        <li class="index_top_nav_ul_li">
+            <a href="https://airrsv.net/AKR5884668016/calendar" target="_blank">
+                <img src="img/index/button_index_top_nav_ul_li_02.svg" alt="お電話でのお問合せは「エクセレントシティ拝島駅前」プロジェクト準備室：10：00～19：00火・水曜定休（祝日は除く）">
+            </a>
+        </li>
+    </ul>
+</nav>
+
+<!--<div class="margin_100 wapper bunner_plan_bottom_wap">
+    <a class="bunner_plan_bottom_link" href="http://www.shinnihon-c.co.jp/zeh-m/" target="_blank">
+        <picture>
+            <source class="lazy" media="(min-width: 768px)" srcset="img/plan/bunner_plan_bottom_link_pc.png">
+            <source class="lazy"  media="(max-width: 768px)" srcset="img/plan/bunner_plan_bottom_link_sp.png">
+            <img src="img/plan/bunner_plan_bottom_link_pc.png" alt="aタイプ">
+        </picture>
+    </a>
+</div>-->
+
+<div class="margin_40 padding_top_30_bottom_30 index_special_movie">
+    <div class="wapper">
+        <a class="index_special_movie_link_box" href="javascript:;" onclick="window.open('movie.php', '', 'width=910,height=800,scrollbars=yes'); return false;">
+            <img src="img/index/button_index_special_movie_link_box.png" alt="スペシャルウォーター">
+        </a>
+    </div>
+</div>
+
+
 <?php include 'include/footer.php'; ?>
