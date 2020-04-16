3880e60ae7339aa28ca017c0611c198d66056039
afiru <yfb19369@gmail.com>
2020-04-16 11:25:41 +0900

    modelroom変更

--- modelroom.php
+++ modelroom.php
@@ -4,7 +4,7 @@
     <div class="text_box">
         <h1 class="t_center color_fff mintyou">MODEL ROOM</h1>
         <p class="mintyou color_fff t_center main_plan_contenr_contents_p">
-            凛とした気品漂う、プライベートレジデンス空間
+            光が踊る。安らぎが満ちる。上質が包み込む、深い寛ぎの時間。
         </p>
     </div>
 </section>
@@ -24,41 +24,55 @@
     <div class="wapper margin_70 modelroom_main_thumbs">
         <div class="modelroom_main_thumbs_contents display_flex_stretch display_row">
             <a class="modelroom_main_thumbs_contents_link" data-slide-index="0" href="#modelroom_main_slider">
-                <img src="img/modelroom/modelroom_main_slider_thumbs_01.png">
+                <img src="img/modelroom/modelroom_main_slider_thumbs_01.jpg">
             </a>
             <a class="modelroom_main_thumbs_contents_link" data-slide-index="1" href="#modelroom_main_slider">
-                <img src="img/modelroom/modelroom_main_slider_thumbs_02.png">
+                <img src="img/modelroom/modelroom_main_slider_thumbs_02.jpg">
             </a>
             <a class="modelroom_main_thumbs_contents_link" data-slide-index="2" href="#modelroom_main_slider">
-                <img src="img/modelroom/modelroom_main_slider_thumbs_03.png">
+                <img src="img/modelroom/modelroom_main_slider_thumbs_03.jpg">
             </a>
             <a class="modelroom_main_thumbs_contents_link" data-slide-index="3" href="#modelroom_main_slider">
-                <img src="img/modelroom/modelroom_main_slider_thumbs_04.png">
+                <img src="img/modelroom/modelroom_main_slider_thumbs_04.jpg">
             </a>
             <a class="modelroom_main_thumbs_contents_link" data-slide-index="4" href="#modelroom_main_slider">
-                <img src="img/modelroom/modelroom_main_slider_thumbs_05.png">
+                <img src="img/modelroom/modelroom_main_slider_thumbs_05.jpg">
             </a>
         </div>
 
         <p class="t_center mintyou modelroom_main_thumbs_bottom_pref">
-            ※掲載写真はモデルルーム（Dタイプ）を撮影（2019年10月）<br class="sp_only">したもので、家具・調度品等は販売価格に含まれません。<br>
-            また、一部オプション（有償）仕様となります。<br class="sp_only">※タイプにより色や形状などが異なります。
+            ※掲載写真はモデルルームを撮影（2020年2月撮影）したものです。家具・調度品等は販売価格に含まれません。一部の仕様はオプション（有償）となります。
         </p>
     </div>
 
 </main>
-<nav class="margin_80 wapper index_top_nav location_bottom_nav">
-    <ul class="display_flex_stretch display_row index_top_nav_ul">
-        <li class="index_top_nav_ul_li">
-            <a href="https://www.ex-ms.com/sendmail/sendmail01.php?no=2070" target="_blank">
-                <img src="img/location/button_index_top_nav_ul_li_01.svg" alt="資料請求">
+<nav class="margin_80 wapper index_top_nav">
+        <ul class="display_flex_stretch display_row index_top_nav_ul">
+            <li class="index_top_nav_ul_li">
+                <a href="https://www.ex-ms.com/sendmail/sendmail01.php?no=2110" target="_blank">
+                    <img src="img/index/button_index_top_nav_ul_li_01.svg" alt="資料請求">
+                </a>
+            </li>
+            <li class="index_top_nav_ul_li">
+                <a href="https://airrsv.net/AKR5884668016/calendar" target="_blank">
+                    <img src="img/index/button_index_top_nav_ul_li_02.svg" alt="お電話でのお問合せは「エクセレントシティ拝島駅前」プロジェクト準備室：10：00～19：00火・水曜定休（祝日は除く）">
+                </a>
+            </li>
+        </ul>
+    </nav>
+<div class="margin_80 padding_top_0_bottom_30 index_special_movie">
+        <div class="wapper">
+            <p class="bnr" style="text-align: center;margin-bottom:10px;">
+                <a href="https://www.ex-ms.com/concept/" target="_blank" style="display:inline-block;    margin: 0 10px;">
+                    <img src="img/banner_policy.jpg" style="max-width:435px; " alt="ブランドポリシー">
+                </a>
+                <a href="https://suumo.jp/jj/guide/shosai/JJ081FD001/?ar=030&hp=007057004" target="_blank" style="display:inline-block;    margin: 0 10px;">
+                    <img src="img/suumo.jpg" style="max-width:435px; " alt="SUUMO 不動産会社ガイド">
+                </a>
+            </p>
+            <a class="index_special_movie_link_box" href="javascript:;" onclick="window.open('movie.php', '', 'width=910,height=800,scrollbars=yes'); return false;">
+                <img src="img/index/button_index_special_movie_link_box.png" alt="エクセレントウォーター動画">
             </a>
-        </li>
-        <li class="index_top_nav_ul_li">
-            <a href="https://airrsv.net/AKR6399203893/calendar" target="_blank">
-                <img src="img/location/button_index_top_nav_ul_li_02.svg" alt="お電話でのお問合せは「エクセレントシティ南行徳1丁目」ゲストサロン受付時間：10：00～19：00火・水曜定休（祝日は除く）">
-            </a>
-        </li>
-    </ul>
-</nav>
+        </div>
+    </div>
 <?php include 'include/footer.php'; ?>
